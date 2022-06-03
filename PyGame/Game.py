1c0cefeec05d66b806e15a901983109fd95aae3f
danuleca <99686907+danuleca@users.noreply.github.com>
2022-06-03 20:05:23 +0300

    Add files via upload

--- PyGame/Game.py
+++ PyGame/Game.py
@@ -9,7 +9,7 @@ pygame.display.set_caption('AppleTreeGame')
 clock = pygame.time.Clock()
 font = pygame.font.Font("Pixeltype.ttf",50)
 
-score = 0
+score = 9
 x = 520
 y = 430
 vel = 7
@@ -22,6 +22,7 @@ apples_y = 0
 apples = pygame.image.load("apple.png").convert_alpha()
 apples = pygame.transform.scale(apples, (50,50))
 apple_collider = apples.get_rect()
+apple_collider2 = apples.get_rect()
 
 
 background = pygame.image.load('bg.jpg')
@@ -75,6 +76,7 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 quit()
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         if player_index >= len(player_walking_left): player_index = 0
@@ -93,34 +95,86 @@ while True:
        player_rect.x = width
     if player_rect.x < 0:
        player_rect.x = 0
-        #falling apples
-    apple_collider.y += apples_vel
-    if apple_collider.y > height:
-        apple_collider_last = apple_collider.x
-        apple_collider.x = random.randrange(0, width)
-        apple_collider.y = 0
-        diff = 300
-        #difference between apple spawns (min 300px difference between the last spawn)
-        while apple_collider_last + diff >= apple_collider.x and apple_collider_last - diff <= apple_collider.x :
-            apple_collider.x = random.randrange(0, width)
-            apple_collider.y = 0
 
-    #collision check(apples)
-    if player_rect.colliderect(apple_collider):
-        score += 1
-        apple_collider_last = apple_collider.x
-        apple_collider.x = random.randrange(0, width)
-        apple_collider.y = 0
-        diff = 300
-        #difference between apple spawns (min 300px difference between the last spawn)
-        while apple_collider_last + diff >= apple_collider.x and apple_collider_last - diff <= apple_collider.x :
+    #falling apples
+    if score<10:
+        apple_collider.y += apples_vel
+        if apple_collider.y > height:
+            apple_collider_last = apple_collider.x
             apple_collider.x = random.randrange(0, width)
             apple_collider.y = 0
-    scoreboard = font.render("Score: " + str(score), False, "Green")
-    screen.blit(background, (0, 0))
-    screen.blit(player_img, player_rect)
-    screen.blit(apples, (apple_collider.x,apple_collider.y))
-    screen.blit(scoreboard, (20, 20))
+            diff = 300
+            #difference between apple spawns (min 300px difference between the last spawn)
+            while apple_collider_last + diff >= apple_collider.x and apple_collider_last - diff <= apple_collider.x :
+                apple_collider.x = random.randrange(0, width)
+                apple_collider.y = 0
+
+        #collision check(apples)
+        if player_rect.colliderect(apple_collider):
+            score += 1
+            apple_collider_last = apple_collider.x
+            apple_collider.x = random.randrange(0, width)
+            apple_collider.y = 0
+            diff = 300
+            #difference between apple spawns (min 300px difference between the last spawn)
+            while apple_collider_last + diff >= apple_collider.x and apple_collider_last - diff <= apple_collider.x :
+                apple_collider.x = random.randrange(0, width)
+                apple_collider.y = 0
+        scoreboard = font.render("Score: " + str(score), False, "Green")
+        screen.blit(background, (0, 0))
+        screen.blit(player_img, player_rect)
+        screen.blit(apples, (apple_collider.x,apple_collider.y))
+        screen.blit(scoreboard, (20, 20))
+
+    if score>=10:
+        vel = 9
+        apple_collider.y += apples_vel
+        apple_collider2.y += apples_vel
+        if apple_collider.y > height or apple_collider2.y > height:
+            apple_collider_last = apple_collider.x
+            apple_collider.x = random.randrange(0, width)
+            apple_collider.y = 0
+            apple_collider2_last = apple_collider2.x
+            apple_collider2.x = random.randrange(0, width)
+            apple_collider2.y = 0
+            diff = 300
+            #difference between apple spawns (min 300px difference between the last spawn)
+            while apple_collider_last + diff >= apple_collider.x and apple_collider_last - diff <= apple_collider.x or apple_collider2_last + diff >= apple_collider2.x and apple_collider2_last - diff <= apple_collider2.x:
+                apple_collider.x = random.randrange(0, width)
+                apple_collider.y = 0
+                apple_collider2.x = random.randrange(0, width)
+                apple_collider2.y = 0
+        #collision check(apples)
+        if player_rect.colliderect(apple_collider):
+            score += 1
+            apple_collider_last = apple_collider.x
+            apple_collider.x = random.randrange(0, width)
+            apple_collider.y = 0
+            diff = 300
+            #difference between apple spawns (min 300px difference between the last spawn)
+            while apple_collider_last + diff >= apple_collider.x and apple_collider_last - diff <= apple_collider.x :
+                apple_collider.x = random.randrange(0, width)
+                apple_collider.y = 0
+        
+        if player_rect.colliderect(apple_collider2):
+            score += 1
+            apple_collider2_last = apple_collider2.x
+            apple_collider2.x = random.randrange(0, width)
+            apple_collider2.y = 0
+            diff = 300
+            #difference between apple spawns (min 300px difference between the last spawn)
+            while apple_collider2_last + diff >= apple_collider2.x and apple_collider2_last - diff <= apple_collider2.x :
+                apple_collider2.x = random.randrange(0, width)
+                apple_collider2.y = 0
+
+        scoreboard = font.render("Score: " + str(score), False, "Green")
+        screen.blit(background, (0, 0))
+        screen.blit(player_img, player_rect)
+        screen.blit(apples, (apple_collider.x,apple_collider.y))
+        screen.blit(apples, (apple_collider2.x,apple_collider2.y))
+        screen.blit(scoreboard, (20, 20))
+        scoreboard = font.render("Score: " + str(score), False, "Green")
+
     player_img = pygame.image.load("Goblin_idle.gif").convert_alpha()
     player_img = pygame.transform.scale(player_img, (100,100))
     pygame.display.update()
